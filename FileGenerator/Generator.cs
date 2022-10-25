3307042bc279e40c9e6341136569c5a5485f67da
Алексей Рублевский <rubleuski.a@gmail.com>
2022-10-25 21:25:24 +0400

    File generator ready

---
+++ FileGenerator/Generator.cs
@@ -0,0 +1,12 @@
+﻿namespace FileGenerator;
+
+public static class Generator
+{
+    private const string Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static readonly Random Random = new();
+    
+    public static int GenerateNumber() => Random.Next(1000);
+
+    public static string GenerateText() =>
+        new(Enumerable.Repeat(Chars, 10).Select(s => s[Random.Next(s.Length)]).ToArray());
+}
--- FileGenerator/Program.cs
+++ FileGenerator/Program.cs
@@ -1,3 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+using static FileGenerator.Generator;
 
-Console.WriteLine("Hello, World!");
+var fileName = args[0];
+var sizeInBytes = int.Parse(args[1]);
+var enableDuplicates = bool.Parse(args[2]);
+var filePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+
+await using var fs = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None);
+
+var i = 0;
+var textToDuplicate = string.Empty;
+var numberToDuplicate = 0;
+
+while (fs.Length < sizeInBytes)
+{
+    i++;
+    var number = GenerateNumber();
+    var text = GenerateText();
+    await WriteLineAsync(fs, number, text);
+
+    if (!enableDuplicates)
+    {
+        continue;
+    }
+
+    if (i % 10 == 0 && string.IsNullOrEmpty(textToDuplicate))
+    {
+        textToDuplicate = text;
+        numberToDuplicate = number;
+        continue;
+    }
+
+    if (i % 5 == 0 && !string.IsNullOrEmpty(textToDuplicate))
+    {
+        await WriteLineAsync(fs, number, textToDuplicate);
+        textToDuplicate = string.Empty;
+        continue;
+    }
+    
+    if (i % 4 == 0 && numberToDuplicate > 0)
+    {
+        await WriteLineAsync(fs, numberToDuplicate, text);
+        numberToDuplicate = 0;
+    }
+}
+
+ValueTask WriteLineAsync(Stream stream, int number, string text) =>
+    stream.WriteAsync(Encoding.UTF8.GetBytes($"{number}. {text}{Environment.NewLine}"));
